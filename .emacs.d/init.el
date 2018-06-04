09277fd48c4a1b4095ac5d264d8fd47588e7c679
Yusuke Inoue <yusuke.inoue@cpc.ait.kyushu-u.ac.jp>
2018-06-04 17:48:46 +0900

    change package manager cask to el-get

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,17 @@
+;;
+;; el-get setting
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+        (eval-print-last-sexp)))
+;; load install package list
+(load (locate-user-emacs-file "Cask2ElGet"))
 
 ;; base setting
 ;; color set
@@ -7,9 +21,10 @@
 (keyboard-translate ?\C-h ?\C-?)
 
 ;; Cask setting load for Mac and Linux
-(when (or (require 'cask "~/.cask/cask.el" t)
-	  (require 'cask nil t))
-    (cask-initialize))
+;;(when (or (require 'cask "~/.cask/cask.el" t)
+;;	  (require 'cask nil t))
+;;    (cask-initialize))
+
 ;; do not make backup files
 (setq make-backup-files nil)
 
